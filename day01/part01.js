00d36222a91d6e88573e9482527b316fc92c7348
Frederik Kehlet <frederik.kehlet@outlook.dk>
2024-08-26 09:17:22 +0200

    primitive solution to part 1

--- day01/part01.js
+++ day01/part01.js
@@ -1,8 +1,72 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var promises_1 = require("fs/promises");
+var cardinal;
+(function (cardinal) {
+    cardinal[cardinal["N"] = 0] = "N";
+    cardinal[cardinal["E"] = 1] = "E";
+    cardinal[cardinal["S"] = 2] = "S";
+    cardinal[cardinal["W"] = 3] = "W";
+})(cardinal || (cardinal = {}));
 (0, promises_1.readFile)('input.txt').then(function (contents) {
     var line = contents.toString();
     var commands = line.split(', ');
-    console.log(commands);
+    var result = solve(commands);
+    console.log(result);
 });
+var solve = function (commands) {
+    var currentDirection = cardinal.N;
+    var x = 0;
+    var y = 0;
+    commands.forEach(function (command) {
+        var direction = command[0];
+        var length = Number(command.slice(1));
+        switch (currentDirection) {
+            case cardinal.N:
+                if (direction == 'R') {
+                    x = x + length;
+                    currentDirection = cardinal.E;
+                    break;
+                }
+                if (direction == 'L') {
+                    x = x - length;
+                    currentDirection = cardinal.W;
+                    break;
+                }
+            case cardinal.E:
+                if (direction == 'R') {
+                    y = y - length;
+                    currentDirection = cardinal.S;
+                    break;
+                }
+                if (direction == 'L') {
+                    y = y + length;
+                    currentDirection = cardinal.N;
+                    break;
+                }
+            case cardinal.S:
+                if (direction == 'R') {
+                    x = x - length;
+                    currentDirection = cardinal.W;
+                    break;
+                }
+                if (direction == 'L') {
+                    x = x + length;
+                    currentDirection = cardinal.E;
+                    break;
+                }
+            case cardinal.W:
+                if (direction == 'R') {
+                    y = y + length;
+                    currentDirection = cardinal.N;
+                    break;
+                }
+                if (direction == 'L') {
+                    y = y - length;
+                    currentDirection = cardinal.S;
+                    break;
+                }
+        }
+    });
+    return Math.abs((0 - y) + (0 - x));
+};
--- day01/part01.ts
+++ day01/part01.ts
@@ -1,12 +1,77 @@
-import {readFile, writeFile} from 'fs/promises'
+import {readFile} from 'fs/promises'
+
+enum cardinal {
+    N,
+    E,
+    S,
+    W
+}
 
 readFile('input.txt').then(contents => {
     const line: string = contents.toString()
     const commands: string[] = line.split(', ')
-    console.log(commands)
+    let result = solve(commands)
+    console.log(result)
 })
 
 
-const solve = (commands: string[]) : Number => {
-    return -1;
+const solve = (commands: string[]) : number => {
+    let currentDirection: number = cardinal.N
+    let x: number = 0;
+    let y: number = 0;
+    
+    commands.forEach(command => {
+        let direction: string = command[0]
+        let length: number = Number(command.slice(1))
+        
+        switch (currentDirection) {
+            case cardinal.N:
+                if (direction == 'R') {
+                    x = x + length
+                    currentDirection = cardinal.E
+                    break
+                }
+                if (direction == 'L') {
+                    x = x - length
+                    currentDirection = cardinal.W
+                    break
+                }
+            case cardinal.E:
+                if (direction == 'R') {
+                    y = y - length 
+                    currentDirection = cardinal.S
+                    break
+                }
+                if (direction == 'L') {
+                    y = y + length
+                    currentDirection = cardinal.N
+                    break
+                }
+            case cardinal.S:
+                if (direction == 'R') {
+                    x = x - length
+                    currentDirection = cardinal.W
+                    break
+                }
+                if (direction == 'L') {
+                    x = x + length
+                    currentDirection = cardinal.E
+                    break
+                }
+            case cardinal.W:
+                if (direction == 'R') {
+                    y = y + length
+                    currentDirection = cardinal.N
+                    break
+                }
+                if (direction == 'L') {
+                    y = y - length
+                    currentDirection = cardinal.S
+                    break
+                }
+        }
+    })
+
+    return Math.abs((0 - y) + (0 - x))
 }
+
